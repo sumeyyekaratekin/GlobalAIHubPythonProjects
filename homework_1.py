81600dc1a9495d6d12f68684ca387a3a1ee0143d
Sümeyye Karatekin <sumeyyekaratekin96@gmail.com>
2020-12-22 19:36:51 +0300

    Initial commit

---
+++ homework_1.py
@@ -0,0 +1,16 @@
+value1 = input("string bir deger giriniz :  ")
+print(f"Girilen deger : {value1}\t girilen degerin veri tipi : {type(value1)} ")
+
+value2 = int(input("integer bir deger giriniz : "))
+print("Girilen deger {}\t girilen degerin veri tipi : {}".format(value2, type(value2)))
+
+value3 = float(input("float bir deger giriniz : "))
+print("Girilen deger {}\t girilen degerin veri tipi : {}".format(value3, type(value3)))
+
+value4 = bool(input("boolean deger giriniz : "))
+print("Girilen deger {}\t girilen degerin veri tipi : {}".format(value4, type(value4)))
+
+value5 = list(input("Liste için elemanlar giriniz : "))
+print("Girilen deger {}\t girilen degerin veri tipi {}".format(value5, type(value5)))
+
+
